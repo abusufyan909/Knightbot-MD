e965d35ad71dfde52dc58821c975c496bafdb05d
abusufyan909 <abusufyanmoon9876@gmail.com>
2025-03-14 12:57:29 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'SUFYAN', // Your name
-  ownerNumber: '923153876122', //Your number
+  ownerNumber: '923276254377', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
